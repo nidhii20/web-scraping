c7ba08c1c326328ee670f779d35063133baddd86
nidhii20 <117992476+nidhii20@users.noreply.github.com>
2023-05-31 19:04:22 +0530

    Update pandas_import.py

--- pandas_import.py
+++ pandas_import.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+states = ["California","Texas","Florida","New york"]
+population = ["213456","456799","123456","6424689"]
+
+dict_states= {'States': states,'Population': population}
+
+df_states= pd.DataFrame.from_dict(dict_states)
+#print(df_states)
+df_states.to_csv('states.csv',index = False )
