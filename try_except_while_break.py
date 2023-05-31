55438099e7a0ed54e47f3cb3710edd5ce92326e7
nidhii20 <117992476+nidhii20@users.noreply.github.com>
2023-05-31 19:09:04 +0530

    Update try_except_while_break.py

--- try_except_while_break.py
+++ try_except_while_break.py
@@ -1,1 +1,17 @@
+new_list = [2,4,6, 'California']
+
+for element in new_list:
+    try:
+        print(element/2)
+    except:
+        print("The element is not a number")
+
+n=4
+while(n>0):
+    print(n)
+    n=n-1
+    if n == 2:
+        break
+
+print("Loop ended")
 
